259105cef7ce20e6af4e24d1bc6b6423eba41858
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-22 23:50:17 -0300

    variables in JavaScript

---
+++ 14_estruturasJavaScript/script.js
@@ -0,0 +1,33 @@
+// 1 - variáveis
+let nome = "Amanda";
+console.log(nome);
+
+nome = "Amanda Voigt"
+console.log(nome);
+
+const idade = 25;
+console.log(idade);
+
+console.log(typeof nome);
+console.log(typeof idade);
+
+// 2 - mais sobre variáveis
+
+// let 2teste = "inválido"
+// let @teste = "inválido"
+
+let a = 10,
+    b = 20,
+    c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Amanda Voigt";
+const nomeCompleto = "Matheus Martins";
+
+console.log(nomecompleto);
+console.log(nomeCompleto);
+
+let _teste = "ok";
+let $teste = "ok";
+console.log(_teste, $teste);
