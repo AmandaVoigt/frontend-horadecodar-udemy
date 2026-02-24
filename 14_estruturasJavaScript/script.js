d03a5aefed33d38405b21c706205cc1d0ca4c9cb
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-23 21:17:33 -0300

    while repetition structure

--- 14_estruturasJavaScript/script.js
+++ 14_estruturasJavaScript/script.js
@@ -110,3 +110,18 @@ if (userName === "José") {
 } else {
     console.log("Nenhuma condição aceita!");
 }
+
+// 10 - while
+let p = 0;
+
+while (p < 5) {
+    console.log(`Repetindo ${p}`);
+    p = p + 1;
+}
+
+// loop infinito
+let x = 10;
+
+while (x > 5) {
+    console.log(`Imprimindo ${x}`);
+}
