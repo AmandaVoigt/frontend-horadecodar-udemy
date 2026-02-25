99a493f09baa80c5a6a76832c8bbddaafad78f99
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-25 11:03:34 -0300

    arrays and properties

--- 16_arraysEObjetosJavaScript/script.js
+++ 16_arraysEObjetosJavaScript/script.js
@@ -7,3 +7,19 @@ console.log(typeof lista);
 const itens = ["Amanda", true, 2, 4.12, []];
 
 console.log(itens);
+
+// 2 - mais sobre arrays
+const arr = ["a", "b", "c", "d"];
+
+console.log(arr[0]);
+console.log(arr[2]);
+console.log(arr[83]);
+
+// 3 - propriedades
+const numbers = [5, 2, 4,];
+
+console.log(numbers.length);
+console.log(numbers["length"]);
+
+const myName = "Amanda";
+console.log(myName.length);
