6c0cf0e9d473525fba5ab6772e888491f77cd79f
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-25 12:29:24 -0300

    getting to know objects better

--- 16_arraysEObjetosJavaScript/script.js
+++ 16_arraysEObjetosJavaScript/script.js
@@ -81,3 +81,10 @@ const obj2 = {
 Object.assign(obj2, obj);
 console.log(obj2);
 console.log(obj);
+
+// 8 - conhecendo melhor os objetos
+console.log(Object.keys(obj));
+console.log(Object.keys(obj2));
+console.log(Object.keys(car));
+
+console.log(Object.entries(car));
