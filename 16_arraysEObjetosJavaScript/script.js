f6ff6c6e42458c7c3d0dc864e0e112a3d8cefd53
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-25 22:59:35 -0300

    slice

--- 16_arraysEObjetosJavaScript/script.js
+++ 16_arraysEObjetosJavaScript/script.js
@@ -160,3 +160,18 @@ console.log(myElements.lastIndexOf("Abacate"));
 
 console.log(myElements.indexOf("Mamão"));
 console.log(myElements.lastIndexOf("Mamão"));
+
+// 14 - slice
+const testeSlice = ["a", "b", "c", "d", "e", "f"];
+const subArray = testeSlice.slice(2, 4);
+console.log(subArray);
+console.log(testeSlice);
+
+const subArray2 = testeSlice.slice(2, 4 + 1);
+console.log(subArray2);
+
+const subArray3 = testeSlice.slice(10, 20);
+console.log(subArray3);
+
+const subArray4 = testeSlice.slice(2);
+console.log(subArray4);
