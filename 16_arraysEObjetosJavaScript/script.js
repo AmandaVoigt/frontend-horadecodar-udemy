45d363f7a8eaead10f0f09d2a6342553064f8f82
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-26 23:18:01 -0300

    rest operator

--- 16_arraysEObjetosJavaScript/script.js
+++ 16_arraysEObjetosJavaScript/script.js
@@ -249,3 +249,16 @@ console.log(fraseDeCompra);
 const palavra = "Testando ";
 console.log(palavra.repeat(5));
 
+// 23 - rest operator
+const somaInfinita = (...args) => {
+    let total = 0;
+
+    for (let i = 0; i < args.length; i++) {
+        total += args[i];
+    }
+
+    return total;
+};
+
+console.log(somaInfinita(1, 2, 3));
+console.log(somaInfinita(1, 20, 34, 3424, 128812, 12723, 12, 23, 54, 12));
