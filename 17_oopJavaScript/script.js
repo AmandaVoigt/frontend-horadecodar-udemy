f2686f01a41c8fbf0e48b9c51c85beb676912f4a
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-27 22:48:15 -0300

    more classes

--- 17_oopJavaScript/script.js
+++ 17_oopJavaScript/script.js
@@ -121,4 +121,33 @@ const jeff = new CachorroClasse("Jeff", "Labrador");
 console.log(jeff);
 console.log(Object.getPrototypeOf(jeff));
 
-//
+// - mais sobre classes
+class Caminhao {
+  constructor(eixos, cor) {
+    this.eixos = eixos;
+    this.cor = cor;
+  }
+
+  descreverCaminhao() {
+    console.log(`Este caminhão tem ${this.eixos} eixos e é da cor ${this.cor}`);
+  }
+}
+
+const scania = new Caminhao(6, "Vermelha");
+
+console.log(scania);
+
+scania.descreverCaminhao();
+
+Caminhao.motor = 4;
+
+const c2 = new Caminhao(4, "Preta");
+
+console.log(c2);
+console.log(c2.motor);
+
+Caminhao.prototype.motor = 4.0;
+
+const c3 = new Caminhao(6, "Azul");
+
+console.log(c3.motor);
