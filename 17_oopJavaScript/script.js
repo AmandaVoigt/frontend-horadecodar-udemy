10a2dd932c2edd2249c280b579852def62bb2426
Amanda Voigt <amanda02_vg@hotmail.com>
2026-02-27 23:22:15 -0300

    instanceof operator

--- 17_oopJavaScript/script.js
+++ 17_oopJavaScript/script.js
@@ -236,3 +236,12 @@ const shark = new Lobo(4, "Shark");
 
 console.log(shark);
 console.log(shark.patas);
+
+// 15 - instanceof
+console.log(shark instanceof Lobo);
+
+console.log(Lobo instanceof Mamifero);
+
+console.log(new Lobo(4, "teste") instanceof Mamifero);
+
+console.log(new Post("a", "b") instanceof Lobo);
