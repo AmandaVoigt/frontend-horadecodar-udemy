a414631cddb5ef324a214b7de501bda26f768a15
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-01 15:07:58 -0300

    assertions

--- 18_debugJavaScript/scripts.js
+++ 18_debugJavaScript/scripts.js
@@ -96,3 +96,15 @@ try {
 } finally {
   console.log("O código foi executado!");
 }
+
+//  8 - assertion
+function checkArray(arr) {
+  if (arr.length === 0) {
+    throw new Error("O array precisa ter elementos");
+  } else {
+    console.log(`O array tem ${arr.length} elementos`);
+  }
+}
+
+// checkArray([]);
+checkArray([1, 2, 3]);
