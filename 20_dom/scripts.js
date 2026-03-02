ceb04f597a330ce0b6d0ccf0ff937a0641cc6767
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 15:22:56 -0300

    Selecting elements by CSS rules

--- 20_dom/scripts.js
+++ 20_dom/scripts.js
@@ -7,17 +7,26 @@ console.log(document.body.childNodes[1].childNodes);
 
 console.log(document.body.childNodes[1].childNodes[1].textContent);
 
-// 2 - selecionando elemento por tag
+// 2 - selecionando elementos por tag
 const listItens = document.getElementsByTagName("li");
 
 console.log(listItens);
 
-// 3 - selecionando elemento por id
+// 3 - selecionando elementos por id
 const title = document.getElementById("title");
 
 console.log(title);
 
-// 4 - selecionando elemento por classe
+// 4 - selecionando elementos por classe
 const products = document.getElementsByClassName("product");
 
 console.log(products);
+
+// 5 - Selecionando elementos por CSS
+const productsQuery = document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer);
