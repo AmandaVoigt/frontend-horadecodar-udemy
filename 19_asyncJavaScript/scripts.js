0b308ace15020d944bd5c9d971558c2c63bfee15
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 10:48:55 -0300

    setTimeout function

---
+++ 19_asyncJavaScript/scripts.js
@@ -0,0 +1,10 @@
+// 1 - setTimeout
+console.log("Ainda não executou");
+
+setTimeout(function () {
+  console.log("Requisição assíncrona");
+}, 2000);
+
+console.log("Ainda não executou 2");
+
+// 2 - 
