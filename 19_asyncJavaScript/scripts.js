126513e77e553fff59e601ed2286b3dcf1b5655c
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 11:10:18 -0300

    promises

--- 19_asyncJavaScript/scripts.js
+++ 19_asyncJavaScript/scripts.js
@@ -10,8 +10,23 @@ console.log("Ainda não executou 2");
 // 2 - setInterval
 console.log("Ainda não começou");
 
-setInterval(function () {
-  console.log("Intervalo assíncrono");
-}, 3000);
+// setInterval(function () {
+//   console.log("Intervalo assíncrono");
+// }, 3000);
 
 console.log("Ainda não começou 2");
+
+// 3 - promises
+const promessa = Promise.resolve(5 + 5);
+
+console.log("Algum código");
+
+promessa
+  .then((value) => {
+    console.log(`A soma é ${value}`);
+    return value;
+  })
+  .then((value) => value - 1)
+  .then((value) => console.log(`Agora é ${value}`));
+
+console.log("Outro código");
