24fab7ab62837dee7c8a32bc66bc417e81741d92
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 10:52:15 -0300

    setInterval function

--- 19_asyncJavaScript/scripts.js
+++ 19_asyncJavaScript/scripts.js
@@ -7,4 +7,11 @@ setTimeout(function () {
 
 console.log("Ainda não executou 2");
 
-// 2 - 
+// 2 - setInterval
+console.log("Ainda não começou");
+
+setInterval(function () {
+  console.log("Intervalo assíncrono");
+}, 3000);
+
+console.log("Ainda não começou 2");
