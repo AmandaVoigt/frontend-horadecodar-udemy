26f754f8bbfffb0d7481438d4065e1d7db1a8017
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 16:14:22 -0300

    height and width of the elements

--- 20_dom/scripts.js
+++ 20_dom/scripts.js
@@ -71,3 +71,12 @@ firstLink.setAttribute("href", "https://www.google.com");
 console.log(firstLink.getAttribute("href"));
 
 firstLink.setAttribute("target", "_blank");
+
+// 11 - altura e largura dos elementos
+const footer = document.querySelector("footer");
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth);
+console.log(footer.clientHeight);
