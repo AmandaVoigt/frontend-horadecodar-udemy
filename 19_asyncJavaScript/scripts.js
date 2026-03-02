91ef2cda85202fbb0fbbe75abace18f1f80dd855
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-02 11:41:22 -0300

    rejecting promises

--- 19_asyncJavaScript/scripts.js
+++ 19_asyncJavaScript/scripts.js
@@ -1,41 +1,63 @@
-// 1 - setTimeout
-console.log("Ainda não executou");
+// // 1 - setTimeout
+// console.log("Ainda não executou");
 
-setTimeout(function () {
-  console.log("Requisição assíncrona");
-}, 2000);
+// setTimeout(function () {
+//   console.log("Requisição assíncrona");
+// }, 2000);
 
-console.log("Ainda não executou 2");
+// console.log("Ainda não executou 2");
 
-// 2 - setInterval
-console.log("Ainda não começou");
+// // 2 - setInterval
+// console.log("Ainda não começou");
 
-// setInterval(function () {
-//   console.log("Intervalo assíncrono");
-// }, 3000);
+// // setInterval(function () {
+// //   console.log("Intervalo assíncrono");
+// // }, 3000);
 
-console.log("Ainda não começou 2");
+// console.log("Ainda não começou 2");
 
-// 3 - promises
-const promessa = Promise.resolve(5 + 5);
+// // 3 - promises
+// const promessa = Promise.resolve(5 + 5);
 
-console.log("Algum código");
+// console.log("Algum código");
 
-promessa
-  .then((value) => {
-    console.log(`A soma é ${value}`);
-    return value;
-  })
-  .then((value) => value - 1)
-  .then((value) => console.log(`Agora é ${value}`));
+// promessa
+//   .then((value) => {
+//     console.log(`A soma é ${value}`);
+//     return value;
+//   })
+//   .then((value) => value - 1)
+//   .then((value) => console.log(`Agora é ${value}`));
 
-console.log("Outro código");
+// console.log("Outro código");
 
-// 4 - falha na promisse
-Promise.resolve(4 * "asd")
-  .then((n) => {
-    if (Number.isNaN(n)) {
-      throw new Error("Valores Inválidos");
+// // 4 - falha na promisse
+// Promise.resolve(4 * "asd")
+//   .then((n) => {
+//     if (Number.isNaN(n)) {
+//       throw new Error("Valores Inválidos");
+//     }
+//   })
+//   .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 5 - rejeição
+function checkNunber(n) {
+  return new Promise((resolve, reject) => {
+    if (n > 10) {
+      resolve(`O número é maior que 10`);
+    } else {
+      reject(new Error("Número muito baixo"));
     }
-  })
-  .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+  });
+}
+
+const a = checkNunber(20);
+const b = checkNunber(10);
+
+a.then((v) => console.log(`O resultado é ${v}`)).catch((err) =>
+  console.log(`Um erro ocorreu: ${err}`),
+);
+
+b.then((v) => console.log(`O resultado é ${v}`)).catch((err) =>
+  console.log(`Um erro ocorreu: ${err}`),
+);
