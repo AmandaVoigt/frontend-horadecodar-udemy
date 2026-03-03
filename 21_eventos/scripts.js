bb6c1fe02051376f410331450e7d13fab41401d6
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-03 14:16:39 -0300

    removing events

--- 21_eventos/scripts.js
+++ 21_eventos/scripts.js
@@ -5,3 +5,18 @@ btn.addEventListener("click", function () {
   console.log("Clicou aqui!");
 });
 
+// 2 - removendo eventos
+const secondBtn = document.querySelector("#btn");
+
+function imprimirMensagem() {
+  console.log("Teste");
+}
+
+secondBtn.addEventListener("click", imprimirMensagem);
+
+const thirdBtn = document.querySelector("#other-btn");
+
+thirdBtn.addEventListener("click", () => {
+  console.log("Evento removido");
+  secondBtn.removeEventListener("click", imprimirMensagem);
+});
