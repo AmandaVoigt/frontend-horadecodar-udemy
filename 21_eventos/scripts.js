b7df6419ed113946d0ff564498f67387677cf034
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-03 14:37:21 -0300

    propagating events

--- 21_eventos/scripts.js
+++ 21_eventos/scripts.js
@@ -30,3 +30,16 @@ myTitle.addEventListener("click", (event) => {
   console.log(event.pointerType);
   console.log(event.target);
 });
+
+// 4 - propagando eventos
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+  console.log("Evento 1");
+});
+
+btnInsideContainer.addEventListener("click", (e) => {
+  e.stopPropagation();
+  console.log("Evento 2");
+});
