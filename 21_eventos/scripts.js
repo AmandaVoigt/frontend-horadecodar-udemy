3197ab5530867f551b9b5a7400bca34f7ecc01fc
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-03 14:46:42 -0300

    key events

--- 21_eventos/scripts.js
+++ 21_eventos/scripts.js
@@ -53,4 +53,12 @@ a.addEventListener("click", (e) => {
   console.log("Não alterou a página");
 });
 
-// 6 -
+// 6 - eventos de tecla
+document.addEventListener("keyup", (e) => {
+  console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown", (e) => {
+  console.log(`Apertou a tecla ${e.key}`);
+});
+
