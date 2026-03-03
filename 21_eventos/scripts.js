382a142a4ea68d12b08083f331bb909f6b8bcf44
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-03 17:21:31 -0300

    Page load event

--- 21_eventos/scripts.js
+++ 21_eventos/scripts.js
@@ -100,3 +100,12 @@ input.addEventListener("focus", (e) => {
 input.addEventListener("blur", (e) => {
   console.log("Saiu do input");
 });
+
+// 11 - evento de carregamento
+window.addEventListener("load", () => {
+  console.log("A página carregou!");
+});
+
+window.addEventListener("beforeunload", (e) => {
+  e.preventDefault();
+});
