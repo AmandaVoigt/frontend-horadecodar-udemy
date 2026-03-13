5d69a02439119dd09e377a1a26f937dab2cd6cb1
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-12 21:46:05 -0300

    var, let and const

--- 26_javaScriptModerno/scripts.js
+++ 26_javaScriptModerno/scripts.js
@@ -1,1 +1,31 @@
-console.log("teste");
+// var, let e const
+var x = 10;
+var y = 15;
+
+if (y > 10) {
+  var x = 5;
+  console.log(x);
+}
+
+console.log(x);
+
+let a = 10;
+let b = 15;
+
+if (b > 10) {
+  let a = 5;
+  console.log(a);
+}
+
+console.log(a);
+
+function logName() {
+  const name = "Maria";
+  console.log(name);
+}
+
+const name = "Amanda";
+
+logName();
+
+console.log(name);
