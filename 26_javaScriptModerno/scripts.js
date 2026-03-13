3ddf20e4b8db87c38e60fc8721041c9849519945
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-12 23:36:25 -0300

    arrow function

--- 26_javaScriptModerno/scripts.js
+++ 26_javaScriptModerno/scripts.js
@@ -1,4 +1,4 @@
-// var, let e const
+// 1 - var, let e const
 var x = 10;
 var y = 15;
 
@@ -29,3 +29,43 @@ const name = "Amanda";
 logName();
 
 console.log(name);
+
+// 2 - arrow function
+const sum = function (a, b) {
+  return a + b;
+};
+
+const arrowSum = (a, b) => a + b;
+
+console.log(sum(5, 5));
+console.log(arrowSum(5, 5));
+
+const greeting = (name) => {
+  if (name) {
+    return `Hello ${name}!`;
+  } else {
+    return "Hello!";
+  }
+};
+
+console.log(greeting("Amanda"));
+console.log(greeting());
+
+const user = {
+  name: "Lion",
+  sayUserName() {
+    setTimeout(function () {
+      console.log(this);
+      console.log(`Usermane: ${this.name}`);
+    }, 1000);
+  },
+  sayUserNameArrow() {
+    setTimeout(() => {
+      console.log(this);
+      console.log(`Username: ${this.name}`);
+    }, 2000);
+  },
+};
+
+user.sayUserName();
+user.sayUserNameArrow();
