3b46c33b9d5d3cfe679010f669521f202efba3b4
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-13 12:40:44 -0300

    classes

--- 26_javaScriptModerno/scripts.js
+++ 26_javaScriptModerno/scripts.js
@@ -150,3 +150,21 @@ const otherInfos = { km: "1000000", price: 49999 };
 
 const car = { ...carName, ...carBrand, ...otherInfos };
 console.log(car);
+
+// 8 - classes
+class Product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
+
+  productWithDiscount(discount) {
+    return this.price * ((100 - discount) / 100);
+  }
+}
+
+const shirt = new Product("Camisa gola V", 20);
+console.log(shirt);
+
+console.log(shirt.productWithDiscount(10));
+console.log(shirt.productWithDiscount(15));
