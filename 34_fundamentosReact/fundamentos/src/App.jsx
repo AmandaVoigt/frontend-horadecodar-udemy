851c25c87aeab7f12f90b026216d2bd1090b6894
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-27 12:38:22 -0300

    comments

--- 34_fundamentosReact/fundamentos/src/App.jsx
+++ 34_fundamentosReact/fundamentos/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 
+// 2 - importando componente
 import FirstComponent from "./components/FirstComponent";
 
 function App() {
   return (
     <div className="App">
+      {/* 3 - comentário JSX */}
       <h1>Fundamentos do React</h1>
       <FirstComponent />
     </div>
