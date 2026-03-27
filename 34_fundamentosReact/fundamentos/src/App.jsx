18cd5426509d9f66558e522f1a7a6405f8dffdda
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-27 12:06:11 -0300

    importing components

--- 34_fundamentosReact/fundamentos/src/App.jsx
+++ 34_fundamentosReact/fundamentos/src/App.jsx
@@ -1,7 +1,14 @@
-import './App.css'
+import "./App.css";
+
+import FirstComponent from "./components/FirstComponent";
 
 function App() {
-  return
+  return (
+    <div className="App">
+      <h1>Fundamentos do React</h1>
+      <FirstComponent />
+    </div>
+  );
 }
 
-export default App
+export default App;
