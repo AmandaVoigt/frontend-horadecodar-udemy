191d62e7fa33e7eec561d53dbcc9732d49bd998e
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-27 15:09:02 -0300

    event with function

--- 34_fundamentosReact/fundamentos/src/components/Events.jsx
+++ 34_fundamentosReact/fundamentos/src/components/Events.jsx
@@ -1,12 +1,22 @@
-// 6 - eventos
-import React from 'react'
+import React from "react";
 
 const Events = () => {
+  const handleClique = (e) => {
+    console.log(e);
+    console.log("Executou");
+  };
+
   return (
     <div>
-      <button onClick={() => console.log("Testando um evento")}>
+      <div>
+        <button onClick={() => console.log("Testando um evento")}>
         Clique aqui
       </button>
+      </div>
+      {/* 7 - evento com função */}
+      <div>
+        <button onClick={handleClique}>Clique aqui - com função</button>
+      </div>
     </div>
   );
 };
