219d30a48467de83736cf062d91607816b848d1c
Amanda Voigt <amanda02_vg@hotmail.com>
2026-03-31 11:50:28 -0300

    importing images

--- 35_avancandoNoReact/avancando/src/App.jsx
+++ 35_avancandoNoReact/avancando/src/App.jsx
@@ -1,11 +1,16 @@
 import "./App.css";
 
+// 2 - imagem em assets
+import city from "./assets/city.jpg";
+
 function App() {
   return (
     <div className="App">
       <h1>Avançando no React</h1>
+      {/* 1 - imagem em public */}
       <img src="public/img1.jpg" alt="Alguma imagem" />
-      <img src="src/assets/city.jpg" alt="Imagem City" />
+      {/* 2 - imagem em assets */}
+      <img src={city} alt="Cidade" />
     </div>
   );
 }
