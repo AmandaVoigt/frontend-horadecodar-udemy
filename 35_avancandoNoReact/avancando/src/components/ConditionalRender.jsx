4fcffcbe6accc2fee254e6b8f59bfe7d75e7b4a5
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-01 16:45:15 -0300

    conditional rendering

---
+++ 35_avancandoNoReact/avancando/src/components/ConditionalRender.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ConditionalRender = () => {
+  const x = 5;
+
+  return <div>
+    {/* 7 - render condicional */}
+    <h3>Isso será exibido?</h3>
+    {x > 2 && <p>Se x for true sim!</p>}
+  </div>;
+};
+
+export default ConditionalRender;
