b43346fa713f2c5cd5fc27879a4d987b76afaf29
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-01 16:06:18 -0300

    the key property

--- 35_avancandoNoReact/avancando/src/components/ListRender.jsx
+++ 35_avancandoNoReact/avancando/src/components/ListRender.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
+import { useState } from "react"
 
 const ListRender = () => {
-  const [list] = useState(["Amanda", "Matheus", "Maria"])  
+  const [list] = useState(["Amanda", "Matheus", "Maria", "Amanda"]);
+
+  const [users] = useState([
+    { id: 1, name: "Amanda", age: 25 },
+    { id: 2, name: "Matheus", age: 30 },
+    { id: 3, name: "Maria", age: 28 },
+  ]);
 
   return (
     <div>
-        <ul>
-            {list.map((item) => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    </div>
-  )
-}
+      {/* render sem key (não recomendado) */}
+      <ul>
+        {list.map((item, i) => (
+          <li key={i}>{item}</li>
+        ))}
+      </ul>
 
-export default ListRender
+      {/* render com key (recomendado) */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} - {user.age} anos
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default ListRender;
