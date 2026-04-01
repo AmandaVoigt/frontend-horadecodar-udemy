d812cdae7ed9e6a4dcb1dbded3c4045161b867b7
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-01 15:00:32 -0300

    getting to know the useState hook

--- 35_avancandoNoReact/avancando/src/App.jsx
+++ 35_avancandoNoReact/avancando/src/App.jsx
@@ -3,14 +3,19 @@ import "./App.css";
 // 2 - imagem em assets
 import city from "./assets/city.jpg";
 
+// 3 - useState
+import Data from "./components/Data";
+
 function App() {
   return (
-    <div className="App">
+    <div className="App" style={{paddingBottom: "500px"} }>
       <h1>Avançando no React</h1>
       {/* 1 - imagem em public */}
       <img src="public/img1.jpg" alt="Alguma imagem" />
       {/* 2 - imagem em assets */}
       <img src={city} alt="Cidade" />
+      {/* 3 - useState */}
+      <Data />
     </div>
   );
 }
---
+++ 35_avancandoNoReact/avancando/src/components/Data.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const Data = () => {
+  const [someData, setSomeData] = useState(10);
+
+  const [anotherNumber, setAnotherNumber] = useState(15);
+
+  return (
+    <div>
+      <div>
+        <p>Valor: {someData}</p>
+        <button onClick={() => setSomeData(15)}>
+          Mudar variável
+        </button>
+      </div>
+
+      <div>
+        <p>Valor: {anotherNumber}</p>
+        <button onClick={() => setAnotherNumber(20)}>
+          Mudar state
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Data;
