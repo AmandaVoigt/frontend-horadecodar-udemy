fb31b0b24a853bdcf3e10ab68cecc2a2198566c3
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-09 16:31:15 -0300

    blocking unnecessary digits

--- 38_projetoCalculadoraIMCReact/imcCalc/src/components/ImcCalc.jsx
+++ 38_projetoCalculadoraIMCReact/imcCalc/src/components/ImcCalc.jsx
@@ -14,7 +14,21 @@ const ImcCalc = ({ calcImc }) => {
     setHeight("");
   };
 
-  
+  const validDigits = (text) => {
+    return text.replace(/[^0-9,]/g, "");
+  };
+
+  const handleHeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    setHeight(updatedValue);
+  };
+
+  const handleWeightChange = (e) => {
+    const updatedValue = validDigits(e.target.value);
+
+    setWeight(updatedValue);
+  };
 
   return (
     <div>
@@ -29,7 +43,8 @@ const ImcCalc = ({ calcImc }) => {
                 name="height"
                 id="height"
                 placeholder="Exemplo 1,75"
-                onChange={(e) => setHeight(e.target.value)}
+                onChange={(e) => handleHeightChange(e)}
+                value={height}
               />
             </div>
             <div className="form-control">
@@ -39,7 +54,8 @@ const ImcCalc = ({ calcImc }) => {
                 name="weight"
                 id="weight"
                 placeholder="Exemplo 70,5"
-                onChange={(e) => setWeight(e.target.value)}
+                onChange={(e) => handleWeightChange(e)}
+                value={weight}
               />
             </div>
           </div>
