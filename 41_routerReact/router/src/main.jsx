16183388fa3d91a02d2bfd86c98afc6006743cce
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-24 18:50:22 -0300

    creating base component

--- 41_routerReact/router/src/main.jsx
+++ 41_routerReact/router/src/main.jsx
@@ -4,22 +4,36 @@ import "./index.css";
 import App from "./App.jsx";
 import Contact from "./routes/Contact.jsx";
 
+// 1 - configurando router
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 // 2 - página de erro
 import ErrorPage from "./routes/ErrorPage.jsx";
 
-// 1 - configurando router
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// 3 - componente base
+import Home from "./routes/Home.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <ErrorPage />
+    errorElement: <ErrorPage />,
+    // 3 - componente base
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contact",
+        element: <Contact />,
+      }
+    ],
   },
-  {
-    path: "contact",
-    element: <Contact />,
-  },
+  // {
+  //   path: "contact",
+  //   element: <Contact />,
+  // },
 ]);
 
 createRoot(document.getElementById("root")).render(
