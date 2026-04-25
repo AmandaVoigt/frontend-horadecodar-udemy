20bbc32ecae07ebfca781ac3851dd67ca293504b
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-24 22:23:31 -0300

    active link

--- 41_routerReact/route/src/components/Navbar.jsx
+++ 41_routerReact/route/src/components/Navbar.jsx
@@ -1,10 +1,21 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <div>
-      <Link to="/">Home</Link><br />
-      <Link to="/contact">Contatos</Link>
+      {/* <Link to="/">Home</Link><br />
+      <Link to="/contact">Contatos</Link> */}
+      <NavLink to="/" className={({ isActive }) => (isActive ? "active" : "")}>
+        Home
+      </NavLink>
+      <br />
+      <NavLink
+        to="/contact"
+        className={({ isActive }) => (isActive ? "active" : "")}
+      >
+        Contatos
+      </NavLink>
     </div>
   );
 };
