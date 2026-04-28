ab7aaf16808cc84753b2bc2933d57d7eaf91de9e
Amanda Voigt <amanda02_vg@hotmail.com>
2026-04-28 15:33:08 -0300

    displaying the question and option

--- 42_projetoQuizEmReact/quiz/src/App.jsx
+++ 42_projetoQuizEmReact/quiz/src/App.jsx
@@ -1,10 +1,13 @@
-// react, componentes, estáticos
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
-import "./App.css";
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
+import GameOver from "./components/GameOver";
+
+import PickCategory from "./components/PickCategory";
+
+import "./App.css";
 
 function App() {
   const [quizState, dispatch] = useContext(QuizContext);
@@ -13,7 +16,9 @@ function App() {
     <div className="App">
       <h1>Quiz de Programação</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
+      {quizState.gameStage === "End" && <GameOver />}
     </div>
   );
 }
