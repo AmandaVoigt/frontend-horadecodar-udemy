f86efb2c54a51a2057fb69f324cd41e088901f77
Amanda Voigt <amanda02_vg@hotmail.com>
2026-07-28 23:46:00 -0300

    finalizing the project

--- 43_projetoContadorRegressivoReact/countdown/src/App.jsx
+++ 43_projetoContadorRegressivoReact/countdown/src/App.jsx
@@ -1,12 +1,24 @@
-import { Outlet } from "react-router-dom";
-
 import NewYear from "./assets/newyear.jpg";
-
+import { Outlet } from "react-router-dom";
+import { useContext } from "react";
+import { CountdownContext } from "./context/CountdownContext";
 import "./App.css";
 
 function App() {
+  const { event } = useContext(CountdownContext);
+
+  let eventImage = null;
+  if (event) eventImage = event.image;
+
   return (
-    <div className="App" style={{ backgroundImage: `url(${NewYear})` }}>
+    <div
+      className="App"
+      style={
+        eventImage
+          ? { backgroundImage: `url(${eventImage})` }
+          : { backgroundImage: `url(${NewYear})` }
+      }
+    >
       <div className="container">
         <Outlet />
       </div>
