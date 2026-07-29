7f4af71ccd776b0da22ec1e259d2349611d803ea
Amanda Voigt <amanda02_vg@hotmail.com>
2026-07-28 23:22:00 -0300

    consuming the context

--- 43_projetoContadorRegressivoReact/countdown/src/routes/Countdown.jsx
+++ 43_projetoContadorRegressivoReact/countdown/src/routes/Countdown.jsx
@@ -1,14 +1,23 @@
 import Title from "../components/Title";
 import Counter from "../components/Counter";
-
 import useCountdown from "../hooks/useCountdown";
+import { CountdownContext } from "../context/CountdownContext";
+import { Navigate } from "react-router-dom";
+import { useContext } from "react";
 
 const Countdown = () => {
-  const [day, hour, minute, second] = useCountdown("Jan 1, 2027 00:00:00");
+  const { event } = useContext(CountdownContext);
+
+  if (!event) return <Navigate to="/" />;
+
+  const eventTitle = event.title;
+  const eventColor = event.color;
+
+  const [day, hour, minute, second] = useCountdown(event.date);
 
   return (
     <>
-      <Title title="Contagem regressiva para 2027" />
+      <Title title={eventTitle} />
       <div className="countdown-container">
         <Counter title="Dias" number={day} />
         <Counter title="Horas" number={hour} />
