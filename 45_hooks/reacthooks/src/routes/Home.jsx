244fbd85d4b766b5c6b81e7121367bd0ea0c7d75
Amanda Voigt <amanda02_vg@hotmail.com>
2026-08-12 20:27:08 -0300

    useRef

--- 45_hooks/reacthooks/src/routes/Home.jsx
+++ 45_hooks/reacthooks/src/routes/Home.jsx
@@ -1,6 +1,7 @@
 import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
+import HookUseRef from "../components/HookUseRef";
 
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
@@ -19,6 +20,7 @@ const Home = () => {
       <h2>useContext</h2>
       <p>Valor do contexto: {contextValue}</p>
       <hr />
+      <HookUseRef />
     </div>
   );
 };
