4bced8ce555ea14fcb142719e8a34f5288101284
Amanda Voigt <amanda02_vg@hotmail.com>
2026-08-12 13:04:32 -0300

    useContext

--- 45_hooks/reacthooks/src/main.jsx
+++ 45_hooks/reacthooks/src/main.jsx
@@ -6,6 +6,8 @@ import App from "./App.jsx";
 import Contact from "./routes/Contact.jsx";
 import Home from "./routes/Home.jsx";
 
+import HookUseContext from "./components/HookUseContext.jsx";
+
 import {
   createBrowserRouter,
   RouterProvider,
@@ -32,6 +34,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <HookUseContext>
+      <RouterProvider router={router} />
+    </HookUseContext>
   </React.StrictMode>,
 );
--- 45_hooks/reacthooks/src/routes/Home.jsx
+++ 45_hooks/reacthooks/src/routes/Home.jsx
@@ -2,7 +2,12 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 
+import { useContext } from "react";
+import { SomeContext } from "../components/HookUseContext";
+
 const Home = () => {
+  const { contextValue } = useContext(SomeContext);
+
   return (
     <div>
       <h1>Home</h1>
@@ -10,6 +15,10 @@ const Home = () => {
       <HookUseState />
       <HookUseReducer />
       <HookUseEffect />
+      <hr />
+      <h2>useContext</h2>
+      <p>Valor do contexto: {contextValue}</p>
+      <hr />
     </div>
   );
 };
