d3b9a77fada7fa081c30ebfc3dd9ff66222e8c49
Amanda Voigt <amanda02_vg@hotmail.com>
2026-08-12 22:20:25 -0300

    useCallback

--- 45_hooks/reacthooks/src/routes/Home.jsx
+++ 45_hooks/reacthooks/src/routes/Home.jsx
@@ -2,6 +2,7 @@ import HookUseEffect from "../components/HookUseEffect";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseState from "../components/HookUseState";
 import HookUseRef from "../components/HookUseRef";
+import HookUseCallback from "../components/HookUseCallback";
 
 import { useContext } from "react";
 import { SomeContext } from "../components/HookUseContext";
@@ -21,6 +22,7 @@ const Home = () => {
       <p>Valor do contexto: {contextValue}</p>
       <hr />
       <HookUseRef />
+      <HookUseCallback />
     </div>
   );
 };
