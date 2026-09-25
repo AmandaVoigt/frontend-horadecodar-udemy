8bb63d8f30ef7cfcfb677b8873e22df0f6ca0a83
Amanda Voigt <amanda02_vg@hotmail.com>
2026-09-24 21:00:18 -0300

    backend

--- 47_projetoPartyTime/arquivos/backend/app.js
+++ 47_projetoPartyTime/arquivos/backend/app.js
@@ -11,12 +11,11 @@ const conn = require("./db/conn");
 
 conn();
 
-// // Routes
-// const routes = require("./routes/router");
+// Routes
+const routes = require("./routes/router");
 
-// app.use("/api", routes);
+app.use("/api", routes);
 
 app.listen(3000, function () {
   console.log("Servidor Online!");
 });
-
